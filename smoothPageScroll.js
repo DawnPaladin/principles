0647c5b09dacd80822bd2451300ef585655bb5c2
James Harris <computersherpa@gmail.com>
2014-09-20 22:41:28 -0500

    Changed Sunlight quote to use gradients instead of a JPEG. Next step: Implement actual CSS gradients instead of a giant PNG.

--- smoothPageScroll.js
+++ smoothPageScroll.js
@@ -4,8 +4,8 @@ $(function(){
 	
 	var $window = $(window);		//Window object
 	
-	var scrollTime = .7;			//Scroll time
-	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+	var scrollTime = .5;			//Scroll time
+	var scrollDistance = 200;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
 		
 	$window.on("mousewheel DOMMouseScroll", function(event){
 		
